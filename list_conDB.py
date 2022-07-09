692bf80582bff838f3ca5fc38c72e6895ef4bb5c
Oliver Lantwin <oliver.lantwin@cern.ch>
2022-07-09 10:33:28 +0200

    Fix and format list_condDB.py

--- list_conDB.py
+++ list_conDB.py
@@ -4,57 +4,49 @@
 # It reads the geometry file and adds subdetectors with their positions to the condDB
 # Takes about 5 mins to fill the condDB
 ###
-from __future__ import print_function, division
+import sys
+import getopt
 from factory import APIFactory
-import datetime, sys
 
-
-from builtins import range
-import operator
-from argparse import ArgumentParser
-from array import array
-import os,ROOT, getopt
-
-level=0
+level = 0
 try:
-        opts, args = getopt.getopt(sys.argv[1:], "l:",[])
+    opts, args = getopt.getopt(sys.argv[1:], "l:", [])
 except getopt.GetoptError:
-        # print help information and exit:
-        print(' enter -l: level')
-        sys.exit()
-	
+    # print help information and exit:
+    print(" enter -l: level")
+    sys.exit()
+
 for o, a in opts:
-   if o in ("-l",):
-      level = int(a)
+    if o in ("-l",):
+        level = int(a)
 
 # Instantiate an API factory
 api_factory = APIFactory()
 # Call construct_DB_API to get an CDB API instance, the path must lead to a valid config.yml file containing the database configuration
 
-conditionsDB = api_factory.construct_DB_API("/home/eric/conditionsDatabase/config.yml")
+conditionsDB = api_factory.construct_DB_API("config.yml")
 
-detector=""
-count=0
+detector = ""
+count = 0
 
-def showdetectors(detector,count,level): 
-  #print ("showdetectors called. count=",count," level=",level) 
-  # Show all detector names in the database:
-  result = conditionsDB.list_detectors(detector)
-  for sd in result:
-    if detector=="":
-        print ("snd subdetectors:",sd)
-        conditions = conditionsDB.get_conditions_by_tag(sd,'Scifi_1')
-        print("conditions of detector",sd," :",conditions)
+
+def showdetectors(detector, count, level):
+    # Show all detector names in the database:
+    result = conditionsDB.list_detectors(detector)
+    for sd in result:
+        if detector == "":
+            print("snd subdetectors:", sd)
+            conditions = conditionsDB.get_conditions_by_tag(sd, "Scifi_1")
+            print("conditions of detector", sd, " :", conditions)
+        else:
+            print("subdetectors inside", detector, " :", result)
+        if count < level:
+            count += 1
+            for j in range(len(result)):
+                showdetectors(result[j], count, level)
     else:
-        print ("subdetectors inside",detector," :",result)
-    if count < level:
-      count+=1    
-      for j in range(len(result)):
-        showdetectors(result[j],count,level)  
-  else:
-    print ("No more subdetectors below subdetector/channel:",detector)
-  #print ("count=",count,"All subdetectors listed up to level",level,".")
-  return
-  
-showdetectors(detector,count,level)
+        print("No more subdetectors below subdetector/channel:", detector)
+
+
+showdetectors(detector, count, level)
 sys.exit()
