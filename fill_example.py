c14356ccf8761f1e2fc22f91b18faa5c1d9bc069
Oliver Lantwin <oliver.lantwin@cern.ch>
2022-07-14 16:10:43 +0200

    Combine examples

--- fill_example.py
+++ fill_example.py
@@ -1,4 +1,4 @@
-"""Test fill functionality.
+"""Test runDB functionality.
 
 This is an example file to demonstrate how the conditionsDatabase API works
 """
@@ -18,12 +18,14 @@ runDB = api_factory.construct_DB_API("config.yml")
 
 fill_id1 = "ab2cx4"
 fill_id2 = "ab2cx5"
-start_time = datetime.datetime.now() - datetime.timedelta(hours=1)
-end_time = datetime.datetime.now() + datetime.timedelta(hours=1)
-runDB.add_fill(fill_id=fill_id1, start_time=start_time, end_time=end_time)
-runDB.add_fill(fill_id=fill_id2, start_time=start_time, end_time=end_time)
+start_time_fill = datetime.datetime.now() - datetime.timedelta(hours=1)
+end_time_fill = datetime.datetime.now() + datetime.timedelta(hours=1)
+
+# Test fill functionality
+runDB.add_fill(fill_id=fill_id1, start_time=start_time_fill, end_time=end_time_fill)
+runDB.add_fill(fill_id=fill_id2, start_time=start_time_fill, end_time=end_time_fill)
 try:
-    runDB.add_fill(fill_id=fill_id2, start_time=start_time, end_time=end_time)
+    runDB.add_fill(fill_id=fill_id2, start_time=start_time_fill, end_time=end_time_fill)
 except ValueError:
     pass
 
@@ -38,5 +40,18 @@ runDB.add_attributes_to_fill(
 runDB.add_attributes_to_fill(fill_id=fill_id1, B1=1402, B2=1402)
 fill = runDB.get_fill(fill_id=fill_id1)
 print(fill)
+
+# Test run functionality
+run_id = "1234"
+start_time_run = datetime.datetime.now() - datetime.timedelta(hours=0.5)
+end_time_run = datetime.datetime.now() + datetime.timedelta(hours=0.5)
+runDB.add_run(
+    run_id=run_id, fill_id=fill_id1, start_time=start_time_fill, end_time=end_time_fill
+)
+print(runDB.get_run(run_id=run_id))
+print(runDB.list_runs())
+print(runDB.list_runs(fill_id=fill_id1))
+print(runDB.list_runs(fill_id=fill_id2))
+runDB.remove_run(run_id=run_id)
 runDB.remove_fill(fill_id=fill_id1)
 runDB.remove_fill(fill_id=fill_id2)
